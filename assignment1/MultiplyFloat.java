4969cc9d0352997fe64b4931fcd4e4e0e860ea3c
Vaibhav Gaikwad <vaibhavgaikwad@Vaibhavs-MacBook-Air.local>
2024-06-22 18:56:59 +0530

    updation

--- assignment1/MultiplyFloat.java
+++ assignment1/MultiplyFloat.java
@@ -9,7 +9,7 @@ public class MultiplyFloat {
 		public static void main(String[] args) {
 			System.out.println("Enter two numbers");
 			Scanner sc = new Scanner (System.in);
-			float af = sc.nextFloat();
+			float a = sc.nextFloat();
 			float b = sc.nextFloat();
 			float sum = a*b;
 			System.out.println("The sum is: "+ sum);
