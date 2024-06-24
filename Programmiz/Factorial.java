63f8bfb39ac6d181e2ecf700ec0a980701f055eb
Vaibhav Gaikwad <vaibhavgaikwad@Vaibhavs-MacBook-Air.local>
2024-06-24 19:23:02 +0530

    24 jun eve

--- Programmiz/Factorial.java
+++ Programmiz/Factorial.java
@@ -5,7 +5,5 @@ import java.util.Scanner;
 public class Factorial {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner (System.in);
-		System.out.println("Enter a Number of factorial which you want: ");
-		
+	}}
         
