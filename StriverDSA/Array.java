287560ef4d89fbe13a2224e8c08e5e34243a82ee
Vaibhav Gaikwad <vaibhavgaikwad@Vaibhavs-MacBook-Air.local>
2024-06-26 20:10:22 +0530

    26 jun

--- StriverDSA/Array.java
+++ StriverDSA/Array.java
@@ -3,8 +3,11 @@ package StriverDSA;
 public class Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		char a1[] = {'t', 'u', 'f'};
+		
+		int a2[] = {11, 12, 13};
+		
+		System.out.println(a2[0]);
 	}
 
 }
